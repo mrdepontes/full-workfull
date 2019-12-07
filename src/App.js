861405453f41872d47f94ee066ac44d2c59eafc0
Richard Depo <mr.depontes@gmail.com>
2019-12-07 09:01:03 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import config from './config';
+import moment from 'moment'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Environment <code>{config.env}</code> Loaded at <code>{moment().format('MM/DD hh:mm:ss a')}</code>.</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
